5760244cd2bbe6274ca3e3efd6361fa2ec13b3fc
Xaif <huzaifaamirka46@gmail.com>
2018-11-05 12:08:40 +0530

    Create chat.js

---
+++ Crisp/chat.js
@@ -0,0 +1,49 @@
+<button class="but"onclick="$crisp.push(['do', 'chat:open'])">Chat With Us</button>
+
+<style>
+.but {
+position: relative;
+cursor: pointer;
+padding: .3em 1em;
+border: 2px solid;
+Display: inline;
+border-top-width: 2px;
+border-right-width: 2px;
+border-bottom-width: 2px;
+border-left-width: 2px;
+border-top-width: 2px;
+border-right-width: 2px;
+border-bottom-width: 2px;
+border-left-width: 2px;
+border-top-width: 2px;
+border-right-width: 2px;
+border-bottom-width: 2px;
+border-left-width: 2px;
+-webkit-border-radius: 3px;
+-moz-border-radius: 3px;
+border-radius: 3px;
+background-color: transparent;
+background-repeat: no-repeat;
+background-position: center;
+background-size: cover;
+font-size: 17px;
+font-weight: 500;
+line-height: 1.7em !important;
+-webkit-transition: all .2s;
+-moz-transition: all .2s;
+transition: all .2s;
+color: #529cd8 !important;
+border-width: 0px !important;
+font-family: 'Roboto',Helvetica,Arial,Lucida,sans-serif !important;
+background: #fff !important;
+margin-left: 26%;
+}
+
+.but:hover {
+ background-color: #ffffff;
+ color: #529cd8 !important;
+}
+
+
+
+</style>

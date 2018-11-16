9c5853c0440589f4ab4f598f2bbd6d9fda007787
Xaif <huzaifaamirka46@gmail.com>
2018-11-16 19:23:26 +0530

    Create scroller.js

---
+++ scroller.js
@@ -0,0 +1,29 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+<script>
+	//paste this code under head tag or in a seperate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+</script>
+
+<style>
+/* Paste this css to your style sheet file or under head tag */
+/* This only works with JavaScript, 
+if it's not present, don't show loader */
+.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(https://iide.co/wp-content/uploads/2018/11/Preloader_2.gif) center no-repeat #fff;
+}
+</style>
+
+<div class="se-pre-con"></div>
